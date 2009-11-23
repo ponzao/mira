f4238cb9631fb296e31e08fcedd9bea1ba3f6f90
Vesa Marttila <vesa.marttila@helsinki.fi>
2009-11-23 23:40:27 +0200

    Modified to use Scanner, this is really poc stuff

--- src/main/java/org/ponzao/InputParser.java
+++ src/main/java/org/ponzao/InputParser.java
@@ -1,23 +1,35 @@
-package org.ponzao;
-
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.*;
 
 public class InputParser {
     public InputParser() {
 
     }
+//
+//    public String readInput(final DataInputStream input) {
+//        final StringBuffer result = new StringBuffer();
+//
+//        try {
+//            while (input.available() != 0) {
+//                result.append((char)input.readChar());
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return result.toString();
+//    }
+//
 
-    public String readInput(final DataInputStream input) {
+    public String readInput() {
+        final Scanner in = new Scanner(System.in);
         final StringBuffer result = new StringBuffer();
 
-        try {
-            while (input.available() != 0) {
-                result.append(input.readChar());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        while (in.hasNextLine()) {
+            result.append(in.nextLine());
+            result.append('\n');
         }
 
         return result.toString();
@@ -25,7 +37,6 @@ public class InputParser {
 
     public static void main(String args[]) {
         final InputParser inputParser = new InputParser();
-        System.out.println(inputParser.readInput(new DataInputStream(new BufferedInputStream(
-                System.in))));
+        System.out.println(inputParser.readInput());
     }
 }
