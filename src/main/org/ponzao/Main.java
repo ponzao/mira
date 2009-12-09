7914c98b0fe1012ccc0cca127b1d27f1ee36b769
Vesa Marttila <vesa@seagull.(none)>
2009-12-09 17:41:28 +0200

    Removed wrongly placed files.

--- src/main/org/ponzao/Main.java
+++
@@ -1,9 +0,0 @@
-package org.ponzao;
-
-public class Main {
-    public static void main(String args[]) {
-        final AStar as = new AStar(new Grid(new InputParser().readInput()),
-                new BinaryHeap<Node>());
-        as.findRoute();
-    }
-}
